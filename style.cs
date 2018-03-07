9a520deccd5919ed7cce32f040e5ffd57858f080
csslick <digitone@naver.com>
2018-03-07 16:19:14 +0900

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,14 @@
+body{ 
+  background-color: skyblue;
+  height: 3000px;
+  background-image: url(https://pre00.deviantart.net/db52/th/pre/i/2013/009/8/6/sky_castle_by_kapitchu-d5qzwbb.png);
+  background-size: cover;
+}
+.c{
+  position: absolute;
+  width: 400px; height: 252px;
+/*   background: #f80; */
+  background-image: url(http://etc.usf.edu/clippix/pix/single-small-cloud-illustration_medium.png);
+  background-size: cover;
+/*   border-radius: 50%; */
+}
